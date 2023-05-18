36a9652ceaeecb273ddd535c9b19b2e05b2809c3
marshallschiele <marshall.schiele@gmail.com>
2023-05-18 18:50:39 -0400

    Program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+
+/*
+ *        File: git-lab-program.cc
+ *      Author: Marshall Schiele
+ *        Date: 5/18/2023
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
